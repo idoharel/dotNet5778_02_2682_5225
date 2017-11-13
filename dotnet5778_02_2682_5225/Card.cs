b1678a4fa63497fe694a0c1dcda81229f70169b1
Ido Harel <idohr1993@gmail.com>
2017-11-13 18:07:21 +0200

    try

---
+++ dotnet5778_02_2682_5225/Card.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet5778_02_2682_5225
+{
+
+    enum E_color { red, black }
+    //class define card in stack
+    class Card : IComparable
+    {
+        private E_color color;
+        private int number;
+
+        #region property
+        public int Number
+        {
+            get { return number; }
+
+            set
+            {
+                if (number > 1 && number < 15)
+                    number = value;
+            }
+        }
+
+        internal E_color Color { get; set; }
+
+        public string CardName
+        {
+            get
+            {
+                if (number == 11) { return "Jack"; }
+                else if (number == 12) { return "Queen"; }
+                else if (number == 13) { return "King"; }
+                else if (number == 14) { return "Ace"; }
+                else { return number.ToString(); }
+            }
+        }
+        #endregion
+
+        //CTOR
+        public Card(E_color col, int num)
+        {
+            color = col;
+            number = num;
+        }
+
+        //override toString to return name and color card
+        public string toString()
+        {
+            return CardName + color.ToString();
+        }
+
+        //comper between cards
+        public int CompareTo(object obj)
+        {
+            return number.CompareTo(((Card)obj).number);
+        }
+    }
+}
+//
+
+
---
+++ dotnet5778_02_2682_5225/CardStock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet5778_02_2682_5225
+{
+    class CardStock
+    {
+        private List<Card> Cards;
+       
+    }
+}
+//
