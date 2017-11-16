5fc0476a381fc3176deed052325572d364935f08
yohanan <yohananha@gmail.com>
2017-11-16 15:57:50 +0200

    15:57

--- dotnet5778_02_2682_5225/Game.cs
+++ dotnet5778_02_2682_5225/Game.cs
@@ -6,16 +6,17 @@ using System.Threading.Tasks;
 
 namespace dotnet5778_02_2682_5225
 {
-    class Game
+    public partial class Game
     {
         CardStock kupa;
         Player ido, yohanan;
         public void startGame()
         {
-            return;
+            
         }
         public void nothing()
         {
+
         }
 
     }
