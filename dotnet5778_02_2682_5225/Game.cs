580b2349c0002a4616a6f6f308197a6b937f9409
yohanan <yohananha@gmail.com>
2017-11-16 15:40:10 +0200

    try

---
+++ dotnet5778_02_2682_5225/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet5778_02_2682_5225
+{
+    class Game
+    {
+        CardStock kupa;
+        Player ido, yohanan;
+        public void startGame()
+        {
+
+        }
+
+    }
+}
